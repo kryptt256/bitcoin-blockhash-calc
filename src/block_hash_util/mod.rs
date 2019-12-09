7a2eb9647157a8a71d477e0346ab974c5acd0fc0
vvmaster <vvmaster@gmail.com>
2019-12-09 01:40:48 +0100

    added: a function which returns the block data as an array of bytes.

--- src/block_hash_util/mod.rs
+++ src/block_hash_util/mod.rs
@@ -8,3 +8,21 @@ pub struct BlockHeader {
     pub nonce: u32
 }
 
+use std::io::Cursor;
+use byteorder::{BigEndian, ReadBytesExt, LittleEndian, WriteBytesExt};
+
+pub fn get_block_bytes(block_header: BlockHeader) -> Vec<u8> {
+    let mut rprev_block = Cursor::new(block_header.hash_prev_block);
+    let mut rmerkle_root = Cursor::new(block_header.hash_merkle_root);
+    let prev_block = rprev_block.read_u32::<BigEndian>().unwrap();
+    let merkle_root = rmerkle_root.read_u32::<BigEndian>().unwrap();
+
+    let mut wtr: Vec<u8> = vec![];
+    wtr.write_u32::<LittleEndian>(block_header.version).unwrap();
+    wtr.write_u32::<LittleEndian>(prev_block).unwrap();
+    wtr.write_u32::<LittleEndian>(merkle_root).unwrap();
+    wtr.write_u32::<LittleEndian>(block_header.time).unwrap();
+    wtr.write_u32::<LittleEndian>(block_header.bits).unwrap();
+    wtr.write_u32::<LittleEndian>(block_header.nonce).unwrap();
+    wtr
+}
